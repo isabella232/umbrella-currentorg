977b401f767bd2517ae4e11eb4cfcbf11c3f4644
Ben Keith <ben@benlk.com>
2017-02-22 15:21:52 -0500

    Fix fabfile

--- fabfile.py
+++ fabfile.py
@@ -45,7 +45,7 @@ def staging():
     """
     env.settings    = 'staging'
     env.hosts       = [ os.environ[ 'CURRENTORG_STAGING_SFTP_HOST' ], ]   # ssh host for production.
-    env.user        = os.environ[ 'CURRENTORG_STAGING_SFTP_USER' ],       # ssh user for production.
+    env.user        = os.environ[ 'CURRENTORG_STAGING_SFTP_USER' ]        # ssh user for production.
     env.password    = os.environ[ 'CURRENTORG_STAGING_SFTP_PASSWORD' ]    # ssh password for production.
     env.domain      = 'currentorg.staging.wpengine.com'
     env.port        = '2222'
