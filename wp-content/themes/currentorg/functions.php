7cb6401259d55e30860b742f3c8c6ff5dc48624b
Ben Keith <ben@benlk.com>
2017-09-01 17:05:03 -0400

    Switch back to production config for chartbeat

--- wp-content/themes/currentorg/functions.php
+++ wp-content/themes/currentorg/functions.php
@@ -306,8 +306,7 @@ function current_chartbeat() {
 
 		/** CONFIGURATION START **/
 		_sf_async_config.uid = 57004;
-		/** _sf_async_config.domain = 'current.org' */
-		_sf_async_config.domain = 'currentorg.staging.wpengine.com'
+		_sf_async_config.domain = 'current.org';
 		_sf_async_config.useCanonical = true;
 		_cbq = window._cbq = (window._cbq || []);
 		<?php
