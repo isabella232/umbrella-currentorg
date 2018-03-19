c59e067cdf56bd87bb0642c700a06eef5ef49a21
Kay Lima <kay@inn.org>
2018-03-19 16:19:19 -0600

    adding function largo_excerpt from largo/inc/post-tags.php

--- wp-content/themes/currentorg/functions.php
+++ wp-content/themes/currentorg/functions.php
@@ -333,3 +333,44 @@ function current_chartbeat() {
 	<?php
 }
 add_action( 'wp_footer', 'current_chartbeat' );
+
+if ( ! function_exists( 'largo_excerpt' ) ) {
+	function largo_excerpt( $the_post = null, $sentence_count = 5, $use_more = null, $more_link = null, $echo = true, $strip_tags = true, $strip_shortcodes = true ) {
+		if (!empty($use_more))
+			_deprecated_argument(__FUNCTION__, '0.5.1', 'Parameter $use_more is deprecated. Please use null as the argument.');
+		if (!empty($more_link))
+			_deprecated_argument(__FUNCTION__, '0.5.1', 'Parameter $more_link is deprecated. Please use null as the argument.');
+
+		$the_post = get_post($the_post); // Normalize it into a post object
+
+		if (!empty($the_post->post_excerpt)) {
+			// if a post has a custom excerpt set, we'll use that
+			$content = apply_filters('get_the_excerpt', $the_post->post_excerpt);
+		} else if (is_home() && preg_match('/<!--more(.*?)?-->/', $the_post->post_content, $matches) > 0) {
+			// if we're on the homepage and the post has a more tag, use that
+			$parts = explode($matches[0], $the_post->post_content, 2);
+			$content = $parts[0];
+		} else {
+			// otherwise we'll just do our best and make the prettiest excerpt we can muster
+			$content = largo_trim_sentences($the_post->post_content, $sentence_count);
+		}
+
+		// optionally strip shortcodes and html
+		$output = '';
+		if ( $strip_tags && $strip_shortcodes )
+			$output .= strip_tags( strip_shortcodes ( $content ) );
+		else if ( $strip_tags )
+			$output .= strip_tags( $content );
+		else if ( $strip_shortcodes )
+			$output .= strip_shortcodes( $content );
+		else
+			$output .= $content;
+
+		$output = apply_filters('the_content', $output);
+
+		if ( $echo )
+			echo $output;
+
+		return $output;
+	}
+}
