67669bcd8201e77d3250da296aa8dba40ba19391
Ben Keith <ben@benlk.com>
2019-06-26 15:10:54 -0400

    Rewrite a lot of main-box.php to suit our needs

--- wp-content/themes/currentorg/business-directory/main-box.tpl.php
+++ wp-content/themes/currentorg/business-directory/main-box.tpl.php
@@ -9,35 +9,47 @@
 ?>
 <div id="wpbdp-main-box" class="wpbdp-main-box" data-breakpoints='{"tiny": [0,360], "small": [360,560], "medium": [560,710], "large": [710,999999]}' data-breakpoints-class-prefix="wpbdp-main-box">
 
-<?php if ( wpbdp_get_option( 'show-search-listings' ) ) : ?>
-<div class="main-fields box-row cols-2 cf">
-    <form action="<?php echo esc_url( $search_url ); ?>" method="get">
-        <input type="hidden" name="wpbdp_view" value="search" />
-        <?php echo $hidden_fields; ?>
-        <?php if ( ! wpbdp_rewrite_on() ) : ?>
-        <input type="hidden" name="page_id" value="<?php echo wpbdp_get_page_id(); ?>" />
-        <?php endif; ?>
-        <div class="box-col search-fields">
-            <div class="box-row cols-<?php echo $no_cols; ?>">
-                <div class="box-col main-input">
-                    <input type="text" id="wpbdp-main-box-keyword-field" class="keywords-field" name="kw" placeholder="<?php echo esc_attr( _x( 'Find listings for <keywords>', 'main box', 'WPBDM' ) ); ?>" />
-                </div>
-                <?php echo $extra_fields; ?>
-            </div>
-        </div>
-        <div class="box-col submit-btn">
-            <input type="submit" value="<?php echo esc_attr_x( 'Find Listings', 'main box', 'WPBDM' ); ?>" /><br />
-            <a class="advanced-search-link" href="<?php echo $search_url; ?>"><?php echo esc_attr_x( 'Advanced Search', 'main box', 'WPBDM' ); ?></a>
-        </div>
-    </form>
-</div>
+	<?php if ( wpbdp_get_option( 'show-search-listings' ) ) : ?>
+		<div class="main-fields clearfix">
+			<form action="<?php echo esc_url( $search_url ); ?>" method="get" class="row-fluid">
+				<div class="span10 row-fluid bg-grey padding-1">
 
-<div class="box-row separator"></div>
-<?php endif; ?>
+					<div class="span10">
+						<label for="wpbdp-main-box-keyword-field"><?php esc_html_e( 'Search for listings' ); ?></label>
+						<input type="text" id="wpbdp-main-box-keyword-field" class="keywords-field" name="kw" placeholder="<?php echo esc_attr( _x( 'Find listings for <keywords>', 'main box', 'WPBDM' ) ); ?>" />
+						<?php echo $extra_fields; ?>
+					</div>
+					<div class="span2">
+						<input id="main-search" type="submit" class="button" value="<?php echo esc_attr_x( 'Find Listings', 'main box', 'WPBDM' ); ?>" /><br />
+						<a class="advanced-search-link" href="<?php echo $search_url; ?>"><?php echo esc_attr_x( 'Advanced Search', 'main box', 'WPBDM' ); ?></a>
+					</div>
+				</div>
 
-<?php $main_links = wpbdp_main_links( $buttons ); ?>
-<?php if ( $main_links ) : ?>
-<div class="box-row"><?php echo $main_links; ?></div>
-<?php endif; ?>
+				<div class="create-btn span2 padding-1">
+					<?php echo wpbdp_main_links( array( 'create' ) ); ?>
+				</div>
 
+				<input type="hidden" name="wpbdp_view" value="search" />
+				<?php echo $hidden_fields; ?>
+
+				<?php if ( ! wpbdp_rewrite_on() ) : ?>
+					<input type="hidden" name="page_id" value="<?php echo wpbdp_get_page_id(); ?>" />
+				<?php endif; ?>
+			</form>
+		</div>
+
+	<?php endif; ?>
+
+	<?php
+		// This is changed from the plugin-controlled options to some hardcoded options
+		// the plugin-provided markup here is absolutely bonkers
+		// if we had time to redo that I'd write my own <a href=" wpbdp_url('') "> code.
+
+		$main_links = wpbdp_main_links( array( 'directory', 'listings' ) );
+		if ( $main_links ) {
+			?>
+				<div class="box-row main-links"><?php echo $main_links; ?></div>
+			<?php
+		}
+	?>
 </div>
