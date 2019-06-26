0e4db8fbc045353fa8ea8c5c0eea983ded87cb11
Josh Darby <josh@inn.org>
2019-06-26 16:14:49 -0400

    Removed default page title of inherited parent from single listing page

---
+++ wp-content/themes/currentorg/partials/content-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
+	
+	<?php do_action('largo_before_page_header'); ?>
+    
+    <?php if( ! ( $post->post_type == 'page' && array_key_exists( '_wpbdp_listing', $wp_query->query_vars ) ) ): ?>
+                
+        <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <?php edit_post_link(__('Edit This Page', 'largo'), '<h5 class="byline"><span class="edit-link">', '</span></h5>'); ?>
+        </header><!-- .entry-header -->
+    
+    <?php endif; ?>
+	
+	<?php do_action('largo_after_page_header'); ?>
+	
+	<section class="entry-content">
+		
+		<?php do_action('largo_before_page_content'); ?>
+		
+		<?php the_content(); ?>
+		
+		<?php do_action('largo_after_page_content'); ?>
+		
+	</section><!-- .entry-content -->
+	
+</article><!-- #post-<?php the_ID(); ?> -->
