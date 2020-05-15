3d3cac69c96732ac0d4df9e1b240b3352cd2eb64
Ben Keith <ben@benlk.com>
2020-05-15 16:53:06 -0400

    Create the current-ltw-projects plugin shell

---
+++ wp-content/plugins/current-ltw-projects/current-ltw-projects.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Current.org Local That Works Projects
+ * Plugin URI:      https://github.com/INN/umbrella-currentorg/tree/master/wp-content/plugins
+ * Description:     Creates the 'projects' post type powering the Local That Works database
+ * Author:          INN Labs
+ * Author URI:      https://labs.inn.org
+ * Text Domain:     current-ltw-projects
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Current_Ltw_Projects
+ */
+
+// Your code starts here.
