6f4761242d48e0b5e2675aed26003eecff5325ac
Ben Keith <ben@benlk.com>
2020-05-20 00:30:53 -0400

    List project tags, project status, project organization

--- wp-content/themes/currentorg/partials/content-projects-list-item.php
+++ wp-content/themes/currentorg/partials/content-projects-list-item.php
@@ -12,47 +12,25 @@ $args = array (
 	// only used to determine the existence of a youtube_url
 	'values' => get_post_custom( $post->ID ),
 
-	// this should be filtered in the event of a term-specific archive
-	'featured' => false,
-
 	// $show_thumbnail does not control whether or not the thumbnail is displayed;
 	// it controls whether or not the thumbnail is displayed normally.
 	'show_thumbnail' => TRUE,
 	'show_excerpt' => TRUE,
 );
 
-$args = apply_filters( 'largo_content_partial_arguments', $args, get_queried_object() );
-
+// @todo: remove this
 extract( $args );
 
 $entry_classes = 'entry-content';
 
 $show_top_tag = largo_has_categories_or_tags();
 
-if ( $featured ) {
-	$entry_classes .= ' span10 with-hero';
-	$show_thumbnail = FALSE;
-}
+$custom = get_post_custom();
 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix projects-list-item'); ?>>
 
 	<?php
-		// Special treatment for posts that are in the Homepage Featured prominence taxonomy term and have thumbnails
-		if ( $featured && ( has_post_thumbnail() || $values['youtube_url'] ) ) { ?>
-			<header>
-				<div class="hero span12 <?php echo $hero_class; ?>">
-				<?php
-					if( has_post_thumbnail() ){
-						echo( '<a href="' . get_permalink() . '" title="' . the_title_attribute( array( 'before' => __( 'Permalink to', 'largo' ) . ' ', 'echo' => false )) . '" rel="bookmark">' );
-						the_post_thumbnail( 'full' );
-						echo( '</a>' );
-					}
-				?>
-				</div>
-			</header>
-		<?php } // end Homepage Featured thumbnail block
-
 		echo '<div class="' . $entry_classes . '">';
 
 		if ( largo_has_categories_or_tags() ) {
@@ -69,6 +47,43 @@ if ( $featured ) {
 		</h2>
 
 		<?php
+			// we may need to redo these links as search query params instead
+			$status = get_the_terms( get_the_ID(), 'project-status' );
+			$categories = get_the_terms( get_the_ID(), 'project-category' );
+			// @todo this throws errors when no terms are found for this type of post 
+			if ( is_array( $status) && is_array( $status ) ) {
+				$terms = array_merge( $status, $categories );
+			} else if ( is_array( $status ) ) {
+				$terms = $status;
+			} else if ( is_array( $categories ) ) {
+				$terms = $categories;
+			} else {
+				$terms = array();
+			}
+
+			if ( ! empty( $terms ) ) {
+				echo '<ul class="project-tags">';
+				foreach ( $terms as $term ) {
+					printf(
+						'<li class="project-tag %1$s-%2$s"><a href="%3$s">%4$s</a></li>',
+						esc_attr( $term->taxonomy ),
+						esc_attr( $term->slug ),
+						// @todo: make this be a link that triggers the search filter for this term
+						get_term_link( $term ),
+						esc_html( $term->name )
+					);
+				}
+				echo '</ul>';
+			}
+
+			if ( ! empty( $custom['project-organization'][0] ) ) {
+				printf(
+					'<span class="project-organization">%1$s</span>',
+					esc_html( $custom['project-organization'][0] )
+				);
+			}
+
+
 			if ( $show_excerpt ) {
 				largo_excerpt();
 			}
