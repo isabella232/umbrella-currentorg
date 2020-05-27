5bed20c37acc66f4171aafc0aacda53f9cde4297
Ben Keith <ben@benlk.com>
2020-05-27 15:37:02 -0400

    Use $query to get the search query

--- wp-content/themes/currentorg/partials/projects-search-form.php
+++ wp-content/themes/currentorg/partials/projects-search-form.php
@@ -1,5 +1,7 @@
 <?php
 
+$search_query = $query->query_vars['s'];
+
 $qo = get_queried_object();
 if ( is_a( $qo, 'WP_Post_Type' ) ) {
 	$form_url = get_post_type_archive_link( $qo->name );
@@ -11,13 +13,12 @@ if ( is_a( $qo, 'WP_Post_Type' ) ) {
 	$form_url = get_search_link();
 }
 
-
 ?>
 <form class="projects-search-form-search form-search" role="search" method="get" action="<?php echo esc_url( $form_url ); ?>">
 	<div class="project-search-container">
 		<label for="projects-search">
 			<?php esc_html_e( 'Search for:', 'currentorg' ); ?>
-			<input type="text" class="searchbox search-query" value="<?php the_search_query(); ?>" name="projects-search" />
+			<input type="text" class="searchbox search-query" value="<?php echo esc_attr( $search_query ); ?>" name="projects-search" />
 		</label>
 	</div>
 </form>
