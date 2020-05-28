1748e424150369baf9ba8f5b3315006aa4e2fcd5
Ben Keith <ben@benlk.com>
2020-05-28 14:33:14 -0400

    Filter the search options by category

--- wp-content/themes/currentorg/partials/projects-search-form.php
+++ wp-content/themes/currentorg/partials/projects-search-form.php
@@ -25,26 +25,37 @@ if ( is_a( $qo, 'WP_Post_Type' ) ) {
 <form class="projects-search-form-search form-search" role="search" method="get" action="<?php echo esc_url( $form_url ); ?>">
 	<div class="project-search-container">
 		<label for="projects-search">
-			<?php esc_html_e( 'Search for:', 'currentorg' ); ?>
-			<input type="text" class="searchbox search-query" value="<?php echo esc_attr( $search_query ); ?>" name="projects-search" />
+			<span class="visuallyhidden"><?php esc_html_e( 'Search for:', 'currentorg' ); ?></span>
+			<input type="text" class="searchbox search-query" value="<?php echo esc_attr( $search_query ); ?>" name="projects-search" placeholder="<?php esc_attr_e( 'Search', 'currentorg' ); ?>"/>
 		</label>
 		<button type="submit" class="btn btn-submit"><?php esc_html_e( 'Search', 'currentorg' ); ?></button>
-		<label for="projects-search">
-			<?php esc_html_e( 'Limit by organization type:', 'currentorg' ); ?>
+		<label for="project-org-type" class="project-org-type">
 			<?php
+				printf(
+					'<span class="visuallyhidden">%1$s</span>',
+					esc_html__( 'Limit by organization type:', 'currentorg' )
+				);
 				wp_dropdown_categories( array(
-					'show_option_none' => __( 'Not limited', 'currentorg' ),
+					'show_option_none' => __( 'Organization type', 'currentorg' ),
 					'option_none_value' => '',
 					'name' => 'project-org-type',
 					'taxonomy' => 'project-org-type',
 					'selected' => ( isset( $_GET['project-org-type'] ) ) ? (int) $_GET['project-org-type'] : '' ,
 				) );
-				error_log(var_export( $_GET['tax_input'], true));
+			?>
+		</label>
+
+		<details class="project-category">
+			<?php
+				printf(
+					'<summary class="btn">%1$s</summary>',
+					__( 'Categories', 'currentorg' )
+				);
 				wp_terms_checklist( null, array(
 					'taxonomy' => 'project-category',
 					'selected_cats' => ( isset( $_GET['tax_input']['project-category'] ) && is_array( $_GET['tax_input']['project-category'] ) ) ? $_GET['tax_input']['project-category'] : '' ,
 				) );
 			?>
-		</label>
+		</details>
 	</div>
 </form>
