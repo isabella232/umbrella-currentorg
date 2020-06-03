5d89bbfb70d6b3ce651d03ff158926f80a8d0d72
Josh Darby <josh@inn.org>
2020-06-03 10:20:48 -0400

    Mobile functionality for single views

--- wp-content/themes/currentorg/js/current-ltw-projects.js
+++ wp-content/themes/currentorg/js/current-ltw-projects.js
@@ -2,11 +2,23 @@ var $ = jQuery;
 $(document).ready(function(){
 
     current_ltw_projects_load_param_project_if_exists();
-    
-    $( ".projects-list article .entry-title a" ).click(function(e){
+
+    $(document).on('click', '.projects-list article .entry-title a', function(e){
         e.preventDefault();
         var post_id = $(this).data("post-id");
         current_ltw_projects_load_single_project(post_id);
+    });
+
+    $(".projects-single-close").on('click', function(){
+        $(".projects-single-layout").hide();
+    });
+
+    $(document).on('scroll', function(){   
+        if($('.sticky-nav-holder').hasClass('show')) {
+            $('html').addClass('sticky-show');
+        } else {
+            $('html').removeClass('sticky-show');
+        }
     });
 
 });
@@ -24,6 +36,8 @@ function current_ltw_projects_load_single_project( post_id ) {
 
     // actually load the single project
     $(".projects-single-holder").load(ajax_object.ajax_url+"?action=load_more_post&post_id="+post_id);
+    $(".projects-single-layout").addClass('open');
+    $(".projects-single-layout").show();
     
     // return and let's be happy
     return;
