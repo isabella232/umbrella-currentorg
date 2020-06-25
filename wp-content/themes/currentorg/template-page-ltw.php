a7fdd7b8c061e6c4bb7491e14f0f549d8d028d62
Josh Darby <josh@inn.org>
2020-06-25 16:39:58 -0400

    Give same opening code as archive, add in missing wrapper

--- wp-content/themes/currentorg/template-page-ltw.php
+++ wp-content/themes/currentorg/template-page-ltw.php
@@ -3,53 +3,25 @@
  * Template Name: Local That Works Template
  * Description: No sidebars, only logo and page content. Does not include global, main and secondary navs.
  */
+wp_head();
 
+// enqueue stylesheet for this template
+wp_enqueue_style('current-ltw-template-stylesheet'); 
+
+// dequeue nav script since there is no nav here
+wp_dequeue_script('largo-navigation');
+
+$queried_object = get_queried_object();
 ?>
-
-<!DOCTYPE html>
-<!--[if lt IE 7]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
-<!--[if IE 7]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
-<!--[if IE 8]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
-<!--[if IE 9]>    <html <?php language_attributes(); ?> class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-
-	<title>
-		<?php
-			global $page, $paged;
-			wp_title( '|', true, 'right' );
-			bloginfo( 'name' ); // Add the blog name.
-			// Add the blog description for the home/front page.
-			$site_description = get_bloginfo( 'description', 'display' );
-			if ( $site_description && ( is_home() || is_front_page() ) )
-				echo " | $site_description";
-		?>
-	</title>
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-    <?php wp_head(); ?>
-    <?php 
-        // enqueue stylesheet for this template
-        wp_enqueue_style('current-ltw-template-stylesheet'); 
-
-        // dequeue nav script since there is no nav here
-        wp_dequeue_script('largo-navigation');
-    ?>
-</head>
-
 <body <?php body_class(); ?>>
+<div id="page" class="hfeed clearfix">
 
-	<div id="page" class="hfeed clearfix">
-
-        <?php
-            do_action( 'largo_before_header' );
-            get_template_part( 'partials/largo-header' );
-            do_action( 'largo_after_header' );
-        ?>
-
+	<?php
+		do_action( 'largo_before_header' );
+		get_template_part( 'partials/largo-header' );
+		do_action( 'largo_after_header' );
+    ?>
+    <div id="main" class="row-fluid clearfix">
         <div id="content" role="main">
             <?php
                 while ( have_posts() ) : the_post();
@@ -78,6 +50,7 @@
                 endwhile;
             ?>
         </div>
+    </div>
 
 <?php
 // End of page
