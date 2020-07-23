a306b8137c66d71229bd5588ba9c74b5364fa250
Josh Darby <josh@inn.org>
2020-07-23 12:20:43 -0400

    Add in missing meta tags to ltw template

--- wp-content/themes/currentorg/template-page-ltw.php
+++ wp-content/themes/currentorg/template-page-ltw.php
@@ -12,6 +12,29 @@ wp_enqueue_style('current-ltw-template-stylesheet');
 wp_dequeue_script('largo-navigation');
 
 $queried_object = get_queried_object();
+
+?>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<?php
+    // Fallback <title> tag on WP pre 4.1
+    // @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
+    // @since 0.6
+    if ( ! function_exists( '_wp_render_title_tag' ) ) {
+        printf(
+            '<title>%1$s</title>',
+            wp_title( '|', false, 'right' )
+        );
+    }
+?>
+<link rel="profile" href="https://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php
+    if ( is_singular() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+
 ?>
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed clearfix">
