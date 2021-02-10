26a6b596c190dd3b34d6623f3bb1f6936ea4d60d
Tim Mannino <tim@kettul.com>
2021-02-10 13:58:16 +0000

    Initial plugin setup for WP Forms validation plugin

---
+++ wp-content/plugins/wp-forms-field-validation/wp-forms-field-validation.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WP Forms Field Validation
+Description: Provides specific validation criteria for WP Forms fields
+Author: Tim Mannino
+Author URI: https://kettul.com
+Version: 1.0
+Text Domain: wp-forms-field-validation
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
+?>
